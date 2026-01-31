ddb87bd65df3ffe576c3a975104e19e185cf05ec
Estefanía MC <adntrouxui@MacBook-Air-de-Estefania.local>
2026-01-31 22:51:36 +0100

    Refactor App component to improve state management and API fetching logic

--- digimon-guess-game/src/App.jsx
+++ digimon-guess-game/src/App.jsx
@@ -1,40 +1,74 @@
 import './App.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const response = await fetch('https://digimon-api.vercel.app/api/digimon', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-});
-
-const data = await response.json();
-const random = Math.floor(Math.random() * data.length);
 
 function App() {
-  const [randomIndex, setRandomIndex] = useState(random)
+  const [randomIndex, setRandomIndex] = useState(null)
+  const [digimonInfo, setDigimonInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [answer, setAnswer] = useState('')
+  const [message, setMessage] = useState('');
+  
+  useEffect(() => {
+    fetch('https://digimon-api.vercel.app/api/digimon')
+    .then((res) => res.json())
+    .then((data) => {
+      setDigimonInfo(data);
+      const random = Math.floor(Math.random() * data.length);
+      setRandomIndex(random)
+      setIsLoading(false)
+    })
+  }, [])
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    const answer = e.target.answer.value;
-    console.log(answer === data?.[randomIndex]?.name);
+
+    const userGuess = answer.toLowerCase().trim();
+    const realName = digimonInfo[randomIndex]?.name.toLowerCase();
+    
+    userGuess === realName
+    ? setMessage("Has ganado")
+    : setMessage("Inténtalo de nuevo")
+    
+    setAnswer('')
   }
 
   const handleReset = () => {
-    const newRandom = Math.floor(Math.random() * data.length);
-    setRandomIndex(newRandom)
+    const newRandom = Math.floor(Math.random() * digimonInfo.length);
+    setRandomIndex(newRandom);
+    setMessage('');
+    setAnswer('');
   }
 
-  return (
+  const handleChange = (e) => {
+    setAnswer(e.target.value);
+  }
+
+  if (isLoading) {
+    return (
+      <div className="App">
+        <h1>Cargando...</h1>
+      </div>
+    );
+  }
+
+return (
     <>
       <h1>Digimon guessing game</h1>
       <div className="card">
-        <img src={data?.[randomIndex]?.img} />
+        <img src={digimonInfo?.[randomIndex]?.img} />
         <form autoComplete="off" action="submit" onSubmit={handleSubmit}>
           <label>¿Cuál es este digimon?
-            <input type="text" id="answer" name="answer" />
+            <input
+              type="text"
+              id="answer"
+              name="answer"
+              value={answer}
+              onChange={handleChange}
+            />
           </label>
-          <button type="reset" onClick={handleReset}>
+          <p>{message}</p>
+          <button type="button" onClick={handleReset}>
             Reset
           </button>
           <button>
